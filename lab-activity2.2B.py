db6a804ae798cac1670749923a3b28507e837d41
Ian S <iansilber32@gmail.com>
2022-02-03 11:28:40 -0500

    this is mindnumbingly easy i hate college

--- lab-activity2.2B.py
+++ lab-activity2.2B.py
@@ -1,7 +1,20 @@
-# string = input("Word to convert: ")
-# count = int(input("How many letters at the end of the string should be converted? "))
-string = "string"
-count = len(string) - 2
+# string = input("Sentence: ")
+# sub_string = input("Word to look for in sentence: ")
+# list = string.lower().split(" ")
+# count = 0
+# for word in list:
+#     if word == sub_string:
+#         count += 1
+# print("There are {} occurences of '{}' in the sentence".format(count, sub_string))
 
-caps = string[:count] + string[count:].upper()
-print(caps)
+string = input("Sentence: ")
+string = string.replace(" ", "").lower()
+sub_string = input("Word to look for in sentence: ")
+count = 0
+for i, char in enumerate(string):
+    if string[i: i + len(sub_string)] == sub_string:
+        count += 1
+print("There are {} occurences of '{}' in the sentence".format(count, sub_string))
+
+print("There are 3 occurrences of 'because' in the sentence" ==
+      "There are 3 occurrences of 'because' in the sentence")
---
+++ lab-activity2.3.py
@@ -0,0 +1,6 @@
+nums = [34, 9, 32, 91, 58, 13, 77, 21, 56]
+count = int(input("Number of elements to fetch from array: "))
+numbers = []
+for i in range(count):
+    numbers.append(nums[i])
+print(numbers)

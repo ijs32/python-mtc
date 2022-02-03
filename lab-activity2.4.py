a14df5c332f93d1317a86687c4607f037defd987
Ian S <iansilber32@gmail.com>
2022-02-03 11:42:05 -0500

    done with next weeks assignments

---
+++ lab-activity2.4.py
@@ -0,0 +1,9 @@
+num = 61
+if num % 2 == 0:
+    print("Odd")
+days = 32
+if days != 31:
+    print("Not a valid day of the month.")
+character = "c"
+if character not in ["a", "i"]:
+    print(f"The letter '{character}' does not also constitute a word.")
